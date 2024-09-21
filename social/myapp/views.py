2974501292077f7c05da9a03b2fa40c5c874eaac
Gayatri-ajit <gayatriajit5@gmail.com>
2024-09-22 00:32:56 +0530

    happy commit

--- social/myapp/views.py
+++ social/myapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,HttpResponse
+from django.contrib.auth import login as auth_login
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 
 def signup(request):
     if request.user.is_authenticated:
@@ -29,3 +34,21 @@ def signup(request):
             return redirect('/create_profile_page')
     else:
         return render(request,"signup.html")
+    
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method=="POST":
+        login_username=request.POST.get('username', None)
+        user_password=request.POST["password"]
+        user = authenticate(request,username=login_username, password = user_password)
+        if user is not None:
+            auth_login(request, user)
+            messages.add_message(request, messages.INFO, 'You have successfully logged in.')
+            return redirect('/')
+
+        else:
+            messages.add_message(request, messages.INFO, 'Invalid username or password.')
+            return render(request,"base/login.html")
+
+    return render(request,"base/login.html")
